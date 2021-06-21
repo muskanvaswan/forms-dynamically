9378a5f1995d94e0bdd4760152aa8a3db4a26f46
Muskan Vaswan <42072661+muskanvaswan@users.noreply.github.com>
2021-06-21 23:25:42 +0530

    Update forms.js

--- forms.js
+++ forms.js
@@ -13,33 +13,27 @@ function formToJson(formId) {
 
 	</form>
 
-	Suppose there is a form like above in the HTML of any page.
-
-	When formToJson("testForm") is called it should return something like this
+	When formToJson("testForm") is called it will return something like this
 
 	{
-	"formId": "testForm",
-	"elements": [{
-			"elementType": "input",
-			"type": "text",
-			"id": "name",
-			"name": "name",
-			"placeholder": "What is your name"
-		},
-		{
-			"elementType": "input",
-			"type": "number",
-			"id": "age",
-			"name": "age",
-			"class": "agefield"
+		"formId": "testForm",
+		"elements": [	{
+					"elementType": "input",
+					"type": "text",
+					"id": "name",
+					"name": "name",
+					"placeholder": "What is your name"
+				},
+				{
+					"elementType": "input",
+					"type": "number",
+					"id": "age",
+					"name": "age",
+					"class": "agefield"
 
-		}
-	]
-}
-
-	Note : This is just a suggestive JSON. You can define your own structure.
-	The idea is to be able to store a structure of the form in JSON format.
-
+				}
+		]
+	}
 	*/
 
 
